b077a4b733ecfbd6a0509174a5a01526d0358fee
Martin Yonkov Dimitrov <mdimi@softserveinc.com>
2022-10-07 15:09:05 +0300

    sql

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const adminController = require('../controllers/admin')
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
@@ -15,11 +15,10 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct)
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
 
 router.post('/delete-product', adminController.postDeleteProduct);
 
-exports.routes = router;
-
+module.exports = router;
