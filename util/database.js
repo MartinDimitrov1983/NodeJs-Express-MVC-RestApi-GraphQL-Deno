80249c1086318dda54b04e9806552964d00b8342
Martin Yonkov Dimitrov <mdimi@softserveinc.com>
2022-10-11 16:52:05 +0300

    mongo

--- util/database.js
+++ util/database.js
@@ -1,8 +1,29 @@
-const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongodb = require('mongodb');
+const MongoCLient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db
+
+const mongoConnect = callback => {
+  MongoCLient.connect('mongodb+srv://Martin:gEjwFmT3B80UoL8l@cluster0.jbzftrw.mongodb.net/?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    })
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+
+  throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
