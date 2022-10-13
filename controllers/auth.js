1700270d21fb12eb919ed9c050a41157f08e015b
Martin Yonkov Dimitrov <mdimi@softserveinc.com>
2022-10-13 18:55:31 +0300

    sessions

---
+++ controllers/auth.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false,
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    User.findById('6346cc88fa4a8fb4f5c2e797')
+        .exec()
+        .then((user) => {
+            console.log(user)
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save((err) => {
+                console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
+};
